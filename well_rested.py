8dbf26deaf9437447f0798f961c2b118e87b210d
Rei Meytal <reimeytal52@gmail.com>
2026-02-15 18:17:09 +0200

    fixed?

--- well_rested.py
+++ well_rested.py
@@ -2,10 +2,21 @@ import json
 
 class Wrapper:
     def __init__(self, json_path: str) -> None:
+        '''
+        init opens the specified json file, checks its validity according to the format,
+        and parses it, setting all api endpoints to class methods
+        '''
         with open(json_path, "r") as json_file:
-            api = json.load(json_file)
-            for func in list(api["functions"]):
-                setattr(self, func, self.return_zero)
+            self.api = json.load(json_file)
+
+            if not isinstance(self.api, list):
+                raise Exception("Invalid description file format.")
+            
+            for func_entry in self.api:
+                if "function_name" not in func_entry:
+                    raise Exception("No function_name field")
+
+                setattr(self, func_entry["function_name"], self.return_zero)
 
     def return_zero(self, *args, **kwargs) -> int:
         return 0
